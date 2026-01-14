935ac76868a91b31e3a35b6f81b3c4f55ff08d4c
JayZ63 <jayantusrete633@outlook.com>
2026-01-14 23:05:31 +0530

    Removed trailing fwd slashes from update and delete

--- frontend/src/services/taskService.js
+++ frontend/src/services/taskService.js
@@ -4,5 +4,5 @@ const API_URL = `${process.env.NEXT_PUBLIC_API_URL}/api/task`;
 
 export const getAllTasks = async () => axios.get(API_URL);
 export const addTask = async (task) => axios.post(API_URL, task);
-export const updateTask = async (taskId, updatedTask) => axios.put(`${API_URL}/${taskId}/`, updatedTask);
-export const deleteTask = async (taskId) => axios.delete(`${API_URL}/${taskId}/`);
+export const updateTask = async (taskId, updatedTask) => axios.put(`${API_URL}/${taskId}`, updatedTask);
+export const deleteTask = async (taskId) => axios.delete(`${API_URL}/${taskId}`);
